a313ba45460051f154eb13699cb859fbdef13fa9
VELOZOEduardo <velozoeduin@gmail.com>
2023-08-18 11:16:50 -0300

    TareaCompletada_Modulo03

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -65,9 +65,13 @@ function esVocal(letra) {
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
-   if (letra == a){return "Es vocal"}
-   else if (letra.length > 1){return "Dato incorrecto"}
-   else ("Dato incorrecto")
+    if(letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u'){
+      return 'Es vocal'
+  }else if(letra.length > 1){
+      return 'Dato incorrecto'
+  }else{
+      return 'Dato incorrecto'
+  }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
